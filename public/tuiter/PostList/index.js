474439dbff09a447b2c3d23bc5d61eccbc195761
renuk21 <renukaganesh21@gmail.com>
2022-10-14 12:34:04 -0400

    A5 Challenge

--- public/tuiter/PostList/index.js
+++ public/tuiter/PostList/index.js
@@ -2,7 +2,7 @@ import PostItem from "./PostItem.js";
 import posts from "./posts.js";
 const PostItems = () => {
     return (`
-            <ul class="list-group">
+            <ul class="list-group wd-bg-black">
             ${posts.map(post => {
         return (PostItem(post));
     }).join('')
