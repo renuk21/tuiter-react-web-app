bfd2ba68d3ec32b9f17ee1475248292e996c644c
renuk21 <renukaganesh21@gmail.com>
2022-10-19 00:24:02 -0400

    A6 tuiter

---
+++ src/tuiter/home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+const HomeComponent = () => {
+    return(
+        <>
+            <h1>Home</h1>
+        </>
+    );
+};
+export default HomeComponent;
