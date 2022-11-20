6aa0c948a7c6f3847fe8e6f17649f877fb79e85f
renuk21 <renukaganesh21@gmail.com>
2022-11-20 03:16:58 -0500

    A8

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {parse} from "@fortawesome/fontawesome-svg-core";
 
 // const TUITS_API = 'http://localhost:4000/api/tuits';
-// const TUITS_API = 'https://tuiter-node-server-app-cs-5610.herokuapp.com';
+// const TUITS_API = 'https://tuiter-node-server-app-web.herokuapp.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
