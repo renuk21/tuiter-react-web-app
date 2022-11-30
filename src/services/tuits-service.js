f50a101a827df4e405874d32c3bbe2b619a35e13
renuk21 <renukaganesh21@gmail.com>
2022-11-29 22:00:18 -0500

    A9 change api base

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,7 +3,7 @@ import {parse} from "@fortawesome/fontawesome-svg-core";
 
 // const TUITS_API = 'http://localhost:4000/api/tuits';
 // const TUITS_API = 'https://tuiter-node-server-app-web.herokuapp.com/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_API_BASE1;
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
